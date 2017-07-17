83552579ec20b60be820f2fc7623b15433dde8b6
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-17 20:45:33 +0300

    init

--- app.py
+++ app.py
@@ -69,7 +69,13 @@ def send_message(recipient_id, message_text):
         },
         "message": {
             "text": message_text
-        }
+        },
+        "buttons":[
+      {
+        "type":"postback",
+        "title":"Bookmark Item",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD"
+      }]
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
