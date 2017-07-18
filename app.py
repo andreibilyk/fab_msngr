e44b03dfe49a99fddbf127822bb92b4f9156973f
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-18 12:07:15 +0300

    init

--- app.py
+++ app.py
@@ -35,10 +35,12 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    try:
+                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                     message_text = messaging_event["message"]["text"]  # the message's text
+                    except BaseException:
+                     print('error')
 
                     send_message(sender_id, "roger that!")
 
