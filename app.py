df58f59ff8dbf2dfa82e50ab10ac57e742dfa487
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-18 13:47:24 +0300

    init

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def send_message(recipient_id, message_text):
             "id": recipient_id
         },
           "message":{
+    "text":'Оберіть сферу права',
     "attachment":{
       "type":"template",
       "payload":{
