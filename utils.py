d9a5f9a7fb2efd3645003e9e629892cb8929d7f2
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-18 19:30:48 +0300

    init

--- utils.py
+++ utils.py
@@ -68,7 +68,7 @@ def generate_markup(answers,callback,recipient_id):
     i = 0
     for item in list_items:
         i += 1
-        if i == 5:
+        if i == 4:
          return data
         if (len(callback + str(i)) <= 2) or (callback + str(i) == '111') or (callback + str(i) == '112') or (callback + str(i) == '113') or (callback + str(i) == '264'):
          data["message"]["attachment"]["payload"]["elements"].append(
