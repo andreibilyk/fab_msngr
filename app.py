1fdc099a91a4b49d7232e345cf013b6c23323618
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-19 12:29:26 +0300

    init

--- app.py
+++ app.py
@@ -68,10 +68,14 @@ def webhook():
                 if messaging_event.get("postback"):
                  print(messaging_event["postback"]["payload"])
                  if messaging_event["postback"]["payload"] in network:
-                     print(messaging_event["postback"]["payload"])
-                     row = db_worker.select_row("'"+network.get(messaging_event["postback"]["payload"])+"'")
-                     data = utils.generate_markup(row[2],messaging_event["postback"]["payload"],messaging_event["sender"]["id"])
-                     send_message(messaging_event["sender"]["id"], data)
+                     if ((messaging_event["postback"]["payload"])[0] != "m"):
+                      row = db_worker.select_row("'"+network.get(messaging_event["postback"]["payload"])+"'")
+                      data = utils.generate_markup(row[2],messaging_event["postback"]["payload"],messaging_event["sender"]["id"])
+                      send_message(messaging_event["sender"]["id"], data)
+                     elif ((messaging_event["postback"]["payload"])[0] == "m"):
+                      row = db_worker.select_row("'"+network.get((messaging_event["postback"]["payload"])[4:])+"'")
+                      data = utils.generate_markup_more(row[2],(messaging_event["postback"]["payload"])[4:],messaging_event["sender"]["id"])
+                      send_message(messaging_event["sender"]["id"], data)
                 if messaging_event.get("message"):  # someone sent us a message
                     try:
                      sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
--- utils.py
+++ utils.py
@@ -11,6 +11,24 @@ def generate_markup(answers,callback,recipient_id):
     :param wrong_answers: Набор неправильных ответов
     :return: Объект кастомной клавиатуры
     """
+    emoji_pattern = re.compile("["
+                u"\U0001F600-\U0001F64F"  # emoticons
+                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                u"\u200d"
+                u"\u23f3"#⏳
+                u"\u2642"#♂
+                u"\ufe0f"
+                u"\u2699"#⚙️
+                u"\U0001f913"
+                u"\u2640"#♀
+                u"\u2019"
+                u"\u2708"#✈️
+                u"\u2695"#⚕
+                u"\U0001F914"
+                u"\u2716"
+                                   "]+", flags=re.UNICODE)
     data = {
         "recipient": {
             "id": recipient_id
@@ -32,24 +50,7 @@ def generate_markup(answers,callback,recipient_id):
 }
 
     }
-    emoji_pattern = re.compile("["
-            u"\U0001F600-\U0001F64F"  # emoticons
-            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-            u"\U0001F680-\U0001F6FF"  # transport & map symbols
-            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-            u"\u200d"
-            u"\u23f3"#⏳
-            u"\u2642"#♂
-            u"\ufe0f"
-            u"\u2699"#⚙️
-            u"\U0001f913"
-            u"\u2640"#♀
-            u"\u2019"
-            u"\u2708"#✈️
-            u"\u2695"#⚕
-            u"\U0001F914"
-            u"\u2716"
-                               "]+", flags=re.UNICODE)
+
     # Склеиваем правильный ответ с неправильными
     list_items = []
     for item in answers.split(','):
@@ -78,6 +79,99 @@ def generate_markup(answers,callback,recipient_id):
                                    }
                    )
          else:
+          data["message"]["attachment"]["payload"]["elements"].append(
+                         {
+                             "title": item,
+                             "buttons": [
+                                 {
+                                     "title": "Переглянути",
+                                     "type": "postback",
+                                     "payload":callback + str(i)
+                                 }
+                             ]
+                         }
+          )
+        else:
+         data["message"]["attachment"]["payload"]["elements"].append(
+                                   {
+                                       "title": item,
+                                       "buttons": [
+                                           {
+                                               "title": "Переглянути",
+                                               "type": "postback",
+                                               "payload":emoji_pattern.sub(r'', item)
+                                           }
+                                       ]
+                                   }
+                   )
+        if len(list_items)>3:
+         data["message"]["attachment"]["payload"]["buttons"] = [
+{
+"title": "Більше",
+"type": "postback",
+"payload": "more"+callback
+}
+]
+    return data
+
+def generate_markup_more(answers,callback,recipient_id):
+    """
+    Создаем кастомную клавиатуру для выбора ответа
+    :param right_answer: Правильный ответ
+    :param wrong_answers: Набор неправильных ответов
+    :return: Объект кастомной клавиатуры
+    """
+    emoji_pattern = re.compile("["
+                u"\U0001F600-\U0001F64F"  # emoticons
+                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                u"\u200d"
+                u"\u23f3"#⏳
+                u"\u2642"#♂
+                u"\ufe0f"
+                u"\u2699"#⚙️
+                u"\U0001f913"
+                u"\u2640"#♀
+                u"\u2019"
+                u"\u2708"#✈️
+                u"\u2695"#⚕
+                u"\U0001F914"
+                u"\u2716"
+                                   "]+", flags=re.UNICODE)
+    data = {
+        "recipient": {
+            "id": recipient_id
+        },
+          "message": {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "list",
+            "top_element_style": "compact",
+            "elements": [
+                        ],
+
+        }
+    }
+}
+
+    }
+
+    # Склеиваем правильный ответ с неправильными
+    list_items = []
+    for item in answers.split(','):
+        list_items.append(item)
+    # Хорошенько перемешаем все элементы
+    #random.shuffle(list_items)
+    # Заполняем разметку перемешанными элементами
+    list_items = list_items[3:]
+    i = 3
+    for item in list_items:
+        i += 1
+        if i == 8:
+         return data
+        if (len(callback + str(i)) <= 2) or (callback + str(i) == '111') or (callback + str(i) == '112') or (callback + str(i) == '113') or (callback + str(i) == '264'):
           data["message"]["attachment"]["payload"]["elements"].append(
                          {
                              "title": item,
