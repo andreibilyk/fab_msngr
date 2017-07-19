4b43addf45ca3b20fd5b64a0013517bf76a5fd0a
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-19 12:32:11 +0300

    init

--- app.py
+++ app.py
@@ -73,9 +73,12 @@ def webhook():
                       data = utils.generate_markup(row[2],messaging_event["postback"]["payload"],messaging_event["sender"]["id"])
                       send_message(messaging_event["sender"]["id"], data)
                      elif ((messaging_event["postback"]["payload"])[0] == "m"):
-                      row = db_worker.select_row("'"+network.get((messaging_event["postback"]["payload"])[4:])+"'")
-                      data = utils.generate_markup_more(row[2],(messaging_event["postback"]["payload"])[4:],messaging_event["sender"]["id"])
-                      send_message(messaging_event["sender"]["id"], data)
+                      try:
+                       row = db_worker.select_row("'"+network.get((messaging_event["postback"]["payload"])[4:])+"'")
+                       data = utils.generate_markup_more(row[2],(messaging_event["postback"]["payload"])[4:],messaging_event["sender"]["id"])
+                       send_message(messaging_event["sender"]["id"], data)
+                      except BaseException as e :
+                       print(str(e))
                 if messaging_event.get("message"):  # someone sent us a message
                     try:
                      sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
