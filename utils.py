ccfb049df23c28348e15afe38e9ca60253745bd9
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-20 12:23:32 +0300

    init

--- utils.py
+++ utils.py
@@ -96,7 +96,7 @@ def generate_markup(answers,callback,recipient_id,headline):
          "image_url": urls.get(headline)
      }
      )
-    print(urls.get(headline))
+    print(data["message"]["attachment"]["payload"]["elements"])
     i = 0
     for item in list_items:
         i += 1
