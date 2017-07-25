8d52c909f2173edc168c002fd9e53c032069480f
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-25 10:24:13 +0300

    init

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def webhook():
                      if (messaging_event["message"]["quick_reply"]["payload"] == "operator"):
                       users.append([messaging_event["sender"]["id"]])
                       params = {"fields":"first_name,last_name","access_token":os.environ["PAGE_ACCESS_TOKEN"]}
-                      r = request.get("https://graph.facebook.com/v2.6/"+messaging_event["sender"]["id"],params = params)
+                      r = requests.get("https://graph.facebook.com/v2.6/"+messaging_event["sender"]["id"],params = params)
                       print(r.url)
                       print(r.json)
                       if r.status_code != 200:
