52ff761ed526fb27c9f76eb469f965ab71f069a6
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-25 10:49:21 +0300

    init

--- app.py
+++ app.py
@@ -103,7 +103,8 @@ def webhook():
                         params = {"fields":"first_name,last_name","access_token":os.environ["PAGE_ACCESS_TOKEN"]}
                         r = requests.get("https://graph.facebook.com/v2.6/"+messaging_event["sender"]["id"],params = params)
                         print(r.url)
-                        print(r.json["first_name"])
+                        data = json.loads(r.text)
+                        print(data)
                         users.append([r.content["first_name"]+" "+r.content["last_name"],messaging_event["sender"]["id"]])
                         print(users)
                         #here
