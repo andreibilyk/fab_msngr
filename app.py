dd6d1b75f0218c7634f6ffb1ffaeb2c3308076e7
Andriy Bilyk <andriy.bilykdeveloper@gmail.com>
2017-07-26 10:49:22 +0300

    init

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'admin' and password == 'secret'
+    return username == 'pravovyk' and password == 'H$sr^Ub8Bj-/kM}c'
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
